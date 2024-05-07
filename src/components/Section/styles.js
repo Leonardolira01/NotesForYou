15e74eb18d166a53274cc7aeb152c09734881664
leonardolira01 <leolira177@gmail.com>
2024-05-07 16:02:29 -0300

    Children

---
+++ src/components/Section/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  margin: 56px 0 28px;
+
+  > h2 {
+    border-bottom-width: 1px; //Colocar uma linha em baixo;
+    border-bottom-style: solid;// Quero que essa linha sejá sólida;
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    
+    padding-bottom: 16px;// Para a linha não ficar tão colada no texto;
+    margin-bottom: 24px;// Para quem estiver em baixo se afastar um pouco;
+
+    color: ${({ theme }) => theme.COLORS.GRAY_100};// Uma cor para todo mundo;
+    font-size: 20px;
+    font-weight: 400; //Regular;
+
+  }
+`;
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,3 +10,15 @@ grid-template-areas:
 "header"
 "content"
 `;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+`;
