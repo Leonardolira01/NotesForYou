86bb69276680d6439c16e4f0957b4696aca63a0a
leonardolira01 <leolira177@gmail.com>
2024-05-07 16:35:30 -0300

    Componente Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details() {
   return(
@@ -15,6 +16,11 @@ export function Details() {
           <li><a href="#">https://www.linkedin.com/in/leonardoliradev/</a></li>
         </Links>
       </Section> 
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node.js" />
+      </Section>
 
       <Button title="Voltar"/>
     </Container>
