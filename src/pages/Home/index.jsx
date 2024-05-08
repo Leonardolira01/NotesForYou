968c0de473021a48e8919c9062cbad4d5351a17e
leonardolira01 <leolira177@gmail.com>
2024-05-08 12:26:03 -0300

    Botão de criar nota

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi'; //add o icone do botão newnote
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'; //Container=vai envolver toda a aplicação, a página / Brand=Parte do logo/ Menu= o que vai ficar fixo / Search=busca / Content= os conteúdos/ NewNote = botão para uma nova nota; 
 
 import { Header } from '../../components/Header';
@@ -27,7 +28,8 @@ export function Home(){
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
     </Container>
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -63,4 +63,13 @@ export const Content = styled.div`
 export const NewNote = styled.button`
     grid-area: newnote;
     //background: yellow; apenas para TESTE DE MARCAÇÃO 
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-right: 8px;
+    }
 `;
