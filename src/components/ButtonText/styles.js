8ea4c161212d59249bf4b94e609158cea38f40b4
leonardolira01 <leolira177@gmail.com>
2024-05-08 11:44:35 -0300

    Corrigindo alertas do Styled Component no ButtonText

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
   background: none; //sem nenhum background;
-  color: ${({ theme, isActive }) => isActive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100}; // Se isActive estiver ativo é laranja, se não estiver vou aplicar uma outra cor que será GRAY.100  
+  color: ${({ theme, $isactive }) => $isactive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100}; // Se isactive estiver ativo é laranja, se não estiver vou aplicar uma outra cor que será GRAY.100  
 
   border: none; //sem nenhuma borda;
   font-size: 16px;
