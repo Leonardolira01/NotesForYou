6b8984e7892a8dbf4c856f1f29742162f6c4bf25
leonardolira01 <leolira177@gmail.com>
2024-05-09 17:04:28 -0300

    Finalizando o New

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/global';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 
-import theme from './styles/theme';
+import theme from './styles/theme'
 
-import { New } from './pages/New';
+import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,9 @@
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
-import { Textarea } from '../../components/Textarea';
-import { NoteItem } from '../../components/NoteItem';
-import { Section } from '../../components/Section';
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
 import { Container, Form } from './styles';
 
@@ -25,6 +26,15 @@ export function New() {
             <NoteItem value="https://www.linkedin.com/in/leonardoliradev/"/>
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className='tags'>
+              <NoteItem value="React.js"/>
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+          
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
