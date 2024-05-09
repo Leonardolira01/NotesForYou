9bc276f936b8c26512f1e76d29748e2fe89535da
leonardolira01 <leolira177@gmail.com>
2024-05-09 19:26:20 -0300

    Navegação

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.header`
   grid-area: header; //assim o cabeçalho fica fixo;
@@ -17,7 +18,7 @@ export const Container = styled.header`
 
 `;
 
-export const Profile = styled.div`
+export const Profile = styled(Link)`
   display: flex; //Para garantir que a imagem e o texto ficará centralizada,
   align-items: center;
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'; 
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   width: 100%;
@@ -64,10 +65,11 @@ export const Content = styled.div`
     overflow-y: auto; //se eu deixa scroll ele ficará fixo, no auto ele ativa a rolagem sozinho;
 `;
 
-export const NewNote = styled.button`
+export const NewNote = styled(Link)`
     grid-area: newnote;
     //background: yellow; apenas para TESTE DE MARCAÇÃO 
     background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
     border: none;
     display: flex;
     align-items: center;
