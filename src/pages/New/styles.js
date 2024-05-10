28aba5c4ac01f6ddc89d69a4f8371caabff8b03b
leonardolira01 <leolira177@gmail.com>
2024-05-09 23:14:28 -0300

    correção de um erro em 'finalizando o new'

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  height: 100vh;
 
   display: grid;
   grid-template-rows: 105px auto;
