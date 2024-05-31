10e8b84e4686fb27ad65ca6063e48a1e2a3d6058
leonardolira01 <leolira177@gmail.com>
2024-05-31 01:15:42 -0300

    Criando contexto

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './styles/global'
 
-import theme from './styles/theme'
+import { MyContext } from './myContext';
 
-import { Routes } from './routes'
+import theme from './styles/theme';
+
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <MyContext.Provider value={{ name:'Leonardo Lira', email: 'leonardo@email.com' }}>
         <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-
+import { MyContext } from '../../myContext';
 import { Container, Form, Background } from './styles';
 
 export function SignIn(){
+
+  const data = useContext(MyContext);
+  console.log("MEU CONTEXTO =>", data);
+  
   return(
     <Container>
       <Form>
