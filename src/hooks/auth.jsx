4928c7c9a8de8827ad78bcd5101ccf0cb91d8a88
leonardolira01 <leolira177@gmail.com>
2024-06-03 12:01:41 -0300

    SignIn

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,18 +1,43 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { api } from '../services/api';
 
+// Criação do contexto de autenticação
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+
+  // Função de login
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      const { user, token } = response.data;
+
+      api.defaults.headers.authorization = `Bearer $(token)`;
+      setData({ user, token})
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar.");
+      }
+    }
+  }
+
+  // Prover o contexto para os filhos
   return (
-    <AuthContext.Provider value={{ name:'Leonardo Lira', email: 'leonardo@email.com' }}>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
-function useAuth(){
+// Hook para usar o contexto de autenticação
+function useAuth() {
   const context = useContext(AuthContext);
-
   return context;
 }
+
+// Exportações
 export { AuthProvider, useAuth };
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,46 +1,53 @@
-import { FiMail, FiLock } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { FiMail, FiLock} from 'react-icons/fi';
+
+import { Link } from 'react-router-dom'
 
 import { useAuth } from '../../hooks/auth';
 
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
+import {Input} from '../../components/Input';
+import {Button} from '../../components/Button';
 
-import { Container, Form, Background } from './styles';
+import { Container, Form, Background } from "./styles";
 
-export function SignIn(){
+export function SignIn() {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
 
-  const data = useAuth();
-  console.log("MEU CONTEXTO =>", data);
-  
-  return(
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
+  return (
     <Container>
       <Form>
-        <h1>Notes For You</h1>
+        <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Faça seu login</h2>
 
-        <Input 
-          placeholder="E-mail"
-          type="text"
+        <Input
+          placeholder='E-mail'
+          type='text'
           icon={FiMail}
+          onChange={ event => setEmail(event.target.value)}
         />
-       
-        <Input 
-          placeholder="Senha"
-          type="password"
+
+        <Input
+          placeholder='Senha'
+          type='password'
           icon={FiLock}
+          onChange={ event => setPassword(event.target.value)}
         />
+        <Button title='Entrar' onClick={handleSignIn} />
 
-        <Button title="Entrar"/>
-
-        <Link to="/register">
-          Criar conta
-        </Link>
+        <Link to='/register'>Criar conta</Link>
       </Form>
 
       <Background />
+
     </Container>
   );
 }
