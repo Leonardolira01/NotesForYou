8c21a799b2ab4a7112a3919edaa7169fa1856137
leonardolira01 <leolira177@gmail.com>
2024-06-19 14:08:51 -0300

    Remover nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -20,6 +20,15 @@ export function Details() {
     navigate("/");
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
     useEffect(() =>{
       async function fetchNote(){
         const response = await api.get(`/notes/${params.id}`);
@@ -36,7 +45,10 @@ export function Details() {
         data &&
         <main>
         <Content>
-          <ButtonText title="Excluir nota" />
+          <ButtonText 
+            title="Excluir nota" 
+            onClick={handleRemove}
+          />
 
           <h1>
             {data.title}
