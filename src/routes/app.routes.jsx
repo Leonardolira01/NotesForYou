d3c0d81162ee7ddf2da47c6c215d54340dc7464d
leonardolira01 <leolira177@gmail.com>
2024-06-19 17:35:06 -0300

    Tratamento de rotas

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,6 +1,6 @@
 //npm install react-router-dom
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { New } from '../pages/New'
 import { Home } from '../pages/Home'
@@ -14,6 +14,8 @@ export function AppRoutes() {
       <Route path="/new" element={<New />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/details/:id" element={<Details />} />
+
+      <Route path="*" element={<Navigate to="/"/>} />
     </Routes>
   )
 }
